5d41d72930a2cffc12f85f235b91982e81d2ea4e
Oleg <katricholeg@outlook.com>
2020-03-02 20:29:31 +0200

    zip

--- Board/Controllers/ServiceController.cs
+++ Board/Controllers/ServiceController.cs
@@ -11,83 +11,88 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MyBoard.Controllers
 {
-    public class ServiceController : Controller
+
+  public class ServiceController : Controller
+  {
+    private readonly IWebHostEnvironment _hostEnvironment;
+
+    public ServiceController(IWebHostEnvironment hostEnvironment)
     {
-        private readonly IWebHostEnvironment _hostEnvironment;
-
-        public ServiceController(IWebHostEnvironment hostEnvironment)
-        {
-            _hostEnvironment = hostEnvironment;
-        }
-
-        //[HttpGet]
-        public IActionResult Index()
-        {
-            PhotoService ser = new PhotoService();
-
-            PhotoServiceBilder bilder1 = new OneBuilder();
-            Service ps = ser.Result(bilder1);
-            ps.GetResult();
-
-
-            PhotoServiceBilder bilder2 = new TwoBilder();
-            Service ps2 = ser.Result(bilder2);
-            ps2.GetResult();
-
-            return View();
-        }
-
-    
-        public FileResult DownloadZipFile()
-        {
-
-            var fileName = string.Format("{0}_ImageFiles.zip", DateTime.Today.Date.ToString("dd-MM-yyyy") + "_1");
-            var tempOutPutPath = Path.Combine(_hostEnvironment.WebRootPath, "images" + fileName);
-
-            
-            using (ZipOutputStream s = new ZipOutputStream(System.IO.File.Create(@"D:\Examp")))
-            {
-                s.SetLevel(9);   
-
-                byte[] buffer = new byte[4096];
-
-                var ImageList = new List<string>();
-
-               
-                for (int i = 0; i < ImageList.Count; i++)
-                {
-                    ZipEntry entry = new ZipEntry(Path.GetFileName(ImageList[i]));
-                    entry.DateTime = DateTime.Now;
-                    entry.IsUnicodeText = true;
-                    s.PutNextEntry(entry);
-
-                    using (FileStream fs = System.IO.File.OpenRead(ImageList[i]))
-                    {
-                        int sourceBytes;
-                        do
-                        {
-                            sourceBytes = fs.Read(buffer, 0, buffer.Length);
-                            s.Write(buffer, 0, sourceBytes);
-                        } while (sourceBytes > 0);
-                    }
-                }
-                s.Finish();
-                s.Flush();
-                s.Close();
-
-            }
-
-            byte[] finalResult = System.IO.File.ReadAllBytes(@"D:\Examp");
-            if (System.IO.File.Exists(tempOutPutPath))
-                System.IO.File.Delete(tempOutPutPath);
-
-            if (finalResult == null || !finalResult.Any())
-                throw new Exception(String.Format("No Files found with Image"));
-
-            return File(finalResult, "application/zip", fileName);
-
-        }
+      _hostEnvironment = hostEnvironment;
     }
-}
 
+    public IActionResult Index()
+    {
+      PhotoService ser = new PhotoService();
+
+      PhotoServiceBilder bilder1 = new OneBuilder();
+      Service ps = ser.Result(bilder1);
+      ps.GetResult();
+
+
+      PhotoServiceBilder bilder2 = new TwoBilder();
+      Service ps2 = ser.Result(bilder2);
+      ps2.GetResult();
+
+      return View();
+    }
+
+    public FileResult GenerateAndDowloadZip()
+    {
+      var wedRoot = _hostEnvironment.WebRootPath;
+      var fileName = "MyZip.zip";
+      var tempOutput = wedRoot + "/picture/" + fileName;
+
+      using (ZipOutputStream zipOutputStream = new ZipOutputStream(System.IO.File.Create(tempOutput)))
+      {
+        zipOutputStream.SetLevel(9);
+
+        byte[] buffer = new byte[4096];
+
+        var imageList = new List<string>();
+
+        imageList.Add(wedRoot + "/picture/1.jpg");
+        imageList.Add(wedRoot + "/picture/2.jpg");
+        imageList.Add(wedRoot + "/picture/3.jpg");
+        imageList.Add(wedRoot + "/picture/4.jpg");
+
+        for (int i = 0; i < imageList.Count; i++)
+        {
+          ZipEntry entry = new ZipEntry(Path.GetFileName(imageList[i]));
+          entry.DateTime = DateTime.Now;
+          entry.IsUnicodeText = true;
+
+          zipOutputStream.PutNextEntry(entry);
+
+          using FileStream fileStream = System.IO.File.OpenRead(imageList[i]);
+          int sourceByte;
+          do
+          {
+            sourceByte = fileStream.Read(buffer, 0, buffer.Length);
+            zipOutputStream.Write(buffer, 0, sourceByte);
+          } while (sourceByte > 0);
+        }
+        zipOutputStream.Finish();
+        zipOutputStream.Flush();
+        zipOutputStream.Close();
+
+      }
+
+      byte[] finalResult = System.IO.File.ReadAllBytes(tempOutput);
+      if (System.IO.File.Exists(tempOutput))
+      {
+        System.IO.File.Delete(tempOutput);
+      }
+
+      if (finalResult == null || !finalResult.Any())
+      {
+        throw new Exception(String.Format("Nothing Found"));
+      }
+
+      return File(finalResult, "application/zip", fileName);
+    }
+    
+
+  }
+}
 
