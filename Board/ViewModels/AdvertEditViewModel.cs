272e11b3abcc29f5ae1a7873fd7838ec2f3a09e5
Oleg <katricholeg@outlook.com>
2020-03-12 21:44:43 +0200

    checkbox

--- Board/ViewModels/AdvertEditViewModel.cs
+++ Board/ViewModels/AdvertEditViewModel.cs
@@ -7,10 +7,17 @@ namespace MyBoard.ViewModels
   {
     public int Id { get; set; }
 
-    public string  ExistingPhotoPath { get; set; }
+    public string ExistingPhotoPath { get; set; }
 
     public List<string> ImageStrings { get; set; }
 
     public IList<string> PhotoUrlToDelete { get; set; }
+  }
+
+  public class SelectedCheckbox
+  {
+    public string Value { get; set; }
+
+    public bool IsChecked { get; set; }
   }
 }
